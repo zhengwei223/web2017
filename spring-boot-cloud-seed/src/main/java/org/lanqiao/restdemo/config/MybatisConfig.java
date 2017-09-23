045af28da08f01614b5ec2da4c334ad8fec5815b
zhengwei <zhengwei@lanqiao.org>
2017-09-23 09:19:03 +0800

    调整spring-boot和spring-cloud项目至一个目录中，梳理项目依赖关系

--- spring-boot-cloud-seed/src/main/java/org/lanqiao/restdemo/config/MybatisConfig.java
+++
@@ -1,8 +0,0 @@
-package org.lanqiao.restdemo.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-@MapperScan("org.lanqiao.restdemo.repository")
-@Configuration
-public class MybatisConfig {
-}
