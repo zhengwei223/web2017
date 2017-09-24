4e551cdf4aa522a4c86982084627e45d327fc1c3
zhengwei <zhengwei@lanqiao.org>
2017-09-24 16:49:43 +0800

    【使用zookeeper作为服务注册中心，开发服务提供者和消费者】

--- showcase/src/main/java/org/lanqiao/showcase/rpc/dubbo/DubboClient.java
+++ showcase/src/main/java/org/lanqiao/showcase/rpc/dubbo/DubboClient.java
@@ -6,11 +6,15 @@ import java.io.IOException;
 
 public class DubboClient {
   public static void main(String[] args) throws IOException {
-    ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("rpc/dubbo-client-context.xml");
+    ClassPathXmlApplicationContext applicationContext =
+        new ClassPathXmlApplicationContext("rpc_dubbo/dubbo-client-context.xml");
     applicationContext.start();
+    for (int i = 0; i < 10; i++) {
 
-    DubboClientConsumer consumer = (DubboClientConsumer) applicationContext.getBean("consumer");
-    System.out.println(consumer.saySomething());
-
+      HelloService service = (HelloService) applicationContext.getBean("helloService");
+      System.out.println("=============================="+service.greet());
+    }
+    System.out.println("按任意键退出");
+    System.in.read();// 任意键退出
   }
 }
--- showcase/src/main/java/org/lanqiao/showcase/rpc/dubbo/HelloServiceImpl.java
+++ showcase/src/main/java/org/lanqiao/showcase/rpc/dubbo/HelloServiceImpl.java
@@ -3,6 +3,6 @@ package org.lanqiao.showcase.rpc.dubbo;
 public class HelloServiceImpl implements HelloService {
   @Override
   public String greet() {
-    return "hello";
+    return "hello333333333333333333333";
   }
 }
